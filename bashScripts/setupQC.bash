3d5f2e2f4dcd873aa2507cb7e6c00ec9390e44e9
wong-nw <38703762+wong-nw@users.noreply.github.com>
2023-02-27 11:03:26 -0500

    Update setupQC.bash

--- bashScripts/setupQC.bash
+++ bashScripts/setupQC.bash
@@ -7,4 +7,4 @@ conda activate scRNA4
 module load R/4.2
 
 mkdir -p "QC/images/$1"
-Rscript workflow/nw_updates_202209/scrna_QC.R cellRangerOut/ QC/ $1 0.2,0.4,0.6,0.8,1.0,1.2,1.4 hg38 HPCA
+Rscript workflow/scripts/scrna_QC.R cellRangerOut/ QC/ $1 0.2,0.4,0.6,0.8,1.0,1.2,1.4 hg38 HPCA
